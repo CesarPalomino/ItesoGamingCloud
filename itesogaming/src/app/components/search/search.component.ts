df798c6324e6c0a34c7c88e640eb139bbcd861e3
CesarPalomino <is716790@iteso.mx>
2022-03-29 21:39:13 -0600

    actualizacion de iconos para poder usar con el json

--- itesogaming/src/app/components/search/search.component.ts
+++ itesogaming/src/app/components/search/search.component.ts
@@ -35,9 +35,9 @@ export class SearchComponent implements OnInit {
       fechaI: "13/03/2050",
       fechaF: "14/03/2050",
       status: "Disponible",
-      plat: "fas fa-desktop",
+      plat: "PC",
       logo: "https://vetealaversh.com/wp-content/uploads/2016/11/darkar.png",
-      costo: "$150",
+      costo: 150,
       ubicacion: "Online",
       hora: "15:00"
     }
@@ -48,10 +48,10 @@ export class SearchComponent implements OnInit {
       juego: "Minecraft",
       fechaI: "28/06/2500",
       fechaF: "30/06/2500",
-      plat: "fab fa-xbox",
+      plat: "Xbox",
       status: "En Curso",
       logo: "https://png.vector.me/files/images/2/8/286710/minecraft_block_preview",
-      costo: "$10000",
+      costo: 10000,
       ubicacion: "Online",
       hora: "12:00"
     }
@@ -62,10 +62,10 @@ export class SearchComponent implements OnInit {
       juego: "Dragon Ball Fighterz",
       fechaI: "25/10/2022",
       fechaF: "02/11/2022",
-      plat: "fab fa-playstation",
+      plat: "PlayStation",
       status: "Terminado",
       logo: "https://image.api.playstation.com/cdn/UP0700/CUSA09072_00/vRai0p5eVj1tN4abFUylDN7enlhcabvL.png",
-      costo: "Gratis",
+      costo: 0,
       ubicacion: "ITESO",
       hora: "16:00"
     }
@@ -76,10 +76,10 @@ export class SearchComponent implements OnInit {
       juego: "Mortal Mortal 11",
       fechaI: "10/12/2040",
       fechaF: "18/12/2040",
-      plat: "fab fa-playstation",
+      plat: "PlayStation",
       status: "En Proceso",
       logo: "https://i.redd.it/uopwhswwywu21.jpg",
-      costo: "$50",
+      costo: 50,
       ubicacion: "en tu casa",
       hora: "18:00"
     }
@@ -90,10 +90,10 @@ export class SearchComponent implements OnInit {
       juego: "Super Smash Bros",
       fechaI: "14/02/2030",
       fechaF: "16/02/2030",
-      plat: "bi bi-nintendo-switch",
+      plat: "Nintendo",
       status: "Disponible",
       logo: "https://cdn.vox-cdn.com/thumbor/m43bhBuTlrUok97DZv4tMjI5lzg=/1400x1400/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/10387783/Screen_Shot_2018-03-08_at_5.33.48_PM.0.png",
-      costo: "Gratis",
+      costo: 0,
       ubicacion: "ITESO",
       hora: "17:00"
     }
